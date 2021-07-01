e8b2740dc90869bb334b63919b2ed2b3ca9ab873
Ivan Rublev <ivan@ivanrublev.me>
2021-07-01 02:50:08 +0200

    Remove raise from secret_key_base

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,12 +1,6 @@
 import Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
-
+secret_key_base = System.get_env("SECRET_KEY_BASE")
 port = String.to_integer(System.get_env("PORT") || "4000")
 
 config :twitter_wall, TwitterWallWeb.Endpoint,
